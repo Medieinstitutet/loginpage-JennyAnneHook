6f296ad8a2f23644b436c9650d9a6ce988f049b8
Jenny Anne Höök <93882622+JennyAnneHook@users.noreply.github.com>
2021-12-05 23:56:07 +0100

    litemer

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ const headerLogo = document.getElementById("logo");
 header.style.backgroundColor = "white";
 headerLogo.innerHTML = "Välkommen till oss, vänligen logga in!";
 
+//för logga in
+
 const loginBtn = document.getElementById("loginBtn");
 loginBtn.innerHTML = "Logga in!"; 
 
+//För logga ut
+
+const logoutBtn = document.getElementById("logoutBtn")
+
+const loginsection = document.querySelector("#Inloggad")
+
 footer.style.backgroundColor = "white";
 footer.innerHTML = "Ha det gött - häjjj!";
-
-
 
 //här är de sanna inloggningsuppgifterna: 
 
@@ -24,17 +30,24 @@ if(inloggad==="true"){
     console.log("Kul att se dig igen, du är nu inloggad!");
 }
 loginBtn.addEventListener("click", logincheck);
+//  logOutBtn.addEventListener("click", () => {
+//     localStorage.clear();
+//     location.reload();
 
 function logincheck(){
     let usernameinput=document.getElementById("username").value; 
     let passwordinput=document.getElementById("Password").value;
-    if(usernameinput===username&&passwordinput===password){
+    if(usernameinput === username && passwordinput === password){
         console.log("Du är inloggad");
         localStorage.setItem("inloggad", true);        //sparar till LS när uppgifterna är rätt
         localStorage.getItem("inloggad");
-
+        loginsection.innerHTML="";
+        loginBtn.classList.add("hide");
+        logoutBtn.style.display="block";
+        
        //vid fel uppgifter:     
     }else{                                                                   
         console.log("Sorry, fel användarnamn eller lösenord, försök igen! ");
     }
 }
+
